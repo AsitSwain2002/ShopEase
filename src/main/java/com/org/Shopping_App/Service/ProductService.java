31e112a5ddb52f085f9918793f950e5d4d77aa7c
AsitSwain2002 <asitswain7327@gmil.com>
2024-08-27 14:40:24 +0530

    Product Module Completed

--- src/main/java/com/org/Shopping_App/Service/ProductService.java
+++ src/main/java/com/org/Shopping_App/Service/ProductService.java
@@ -1,5 +1,9 @@
 package com.org.Shopping_App.Service;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.org.Shopping_App.Dto.ProductsDto;
 
 import jakarta.servlet.http.HttpSession;
@@ -7,4 +11,12 @@ import jakarta.servlet.http.HttpSession;
 public interface ProductService {
 
 	ProductsDto saveProduct(ProductsDto productDto);
+	
+	List<ProductsDto> fetchAllProduct();
+	 
+	void removeProduct(int id);
+	
+	ProductsDto findById(int id);
+	
+	ProductsDto updateProduct(ProductsDto productsDto , int id , MultipartFile file);
 }
